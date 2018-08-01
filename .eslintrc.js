c293b872f95a48123e2d70322e2a53a8321feada
Christian Zwergius <christian.zwergius@keytree.es>
2018-08-01 08:48:39 +0200

    Initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  rules: {
+    'import/prefer-default-export': 0,
+    // 'import/no-default-export': 2,
+    'max-lines': ['error', 300],
+    quotes: ['error', 'single'],
+  },
+}
