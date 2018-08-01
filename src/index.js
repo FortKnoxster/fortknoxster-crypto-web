0890508917bc4be5f77012076006108469c430d7
Christian Zwergius <christian.zwergius@keytree.es>
2018-08-01 09:41:18 +0200

    Revert "test2"
    This reverts commit 5f1d91cf97343cafca6a94a5ccbaf4c1cd0ba3c3.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const defaultAwesomeFunction = name => {
   return returnStr
 }
 
-let testa
+let test
 const awesomeFunction = () => 'I am just an Awesome Function'
 
 export default defaultAwesomeFunction
