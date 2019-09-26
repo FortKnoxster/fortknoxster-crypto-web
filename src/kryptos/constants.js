8a2524e02b6ef37f6e3a440c91dac77b5d31ffdb
mickey <mj@fortknoxster.com>
2019-09-26 17:02:21 +0200

    New contacts

--- src/kryptos/constants.js
+++ src/kryptos/constants.js
@@ -3,6 +3,8 @@ export const NONEXTRACTABLE = false
 export const LENGTH_2048 = 2048
 export const LENGTH_256 = 256
 export const LENGTH_128 = 128
+export const LENGTH_32 = 32
+export const PROTECTOR_ITERATIONS = 20000
 export const PROTECTOR_TYPES = {
   password: 'password',
   recovery: 'recovery_key',
