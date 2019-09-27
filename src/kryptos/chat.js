d8b9f00c6e3a7a1173d1280fa1cbbd0d1b584777
mickey <mj@fortknoxster.com>
2019-09-27 09:34:30 +0200

    Removed Promise

--- src/kryptos/chat.js
+++ src/kryptos/chat.js
@@ -11,29 +11,23 @@ export function initChat(keyStore) {
   chat.keyStore = keyStore
 }
 
-export function encryptChatMessage(plainText, publicKeys) {
-  return new Promise(async (resolve, reject) => {
-    const { keyStore } = chat
-    try {
-      const sessionKey = await generateSessionKey(algorithms.AES_CBC_ALGO)
-      const privateKey = await keyStore.getPsk()
-      const result = encryptSign(plainText, sessionKey, privateKey, publicKeys)
-      resolve(result)
-    } catch (error) {
-      reject(error)
-    }
-  })
+export async function encryptChatMessage(plainText, publicKeys) {
+  const { keyStore } = chat
+  try {
+    const sessionKey = await generateSessionKey(algorithms.AES_CBC_ALGO)
+    const privateKey = await keyStore.getPsk()
+    return encryptSign(plainText, sessionKey, privateKey, publicKeys)
+  } catch (error) {
+    return Promise.reject(error)
+  }
 }
 
-export function encryptGroupChatMessage(plainText, sessionKey) {
-  return new Promise(async (resolve, reject) => {
-    const { keyStore } = chat
-    try {
-      const privateKey = await keyStore.getPsk()
-      const result = encryptSign(plainText, sessionKey, privateKey)
-      resolve(result)
-    } catch (error) {
-      reject(error)
-    }
-  })
+export async function encryptGroupChatMessage(plainText, sessionKey) {
+  const { keyStore } = chat
+  try {
+    const privateKey = await keyStore.getPsk()
+    return encryptSign(plainText, sessionKey, privateKey)
+  } catch (error) {
+    return Promise.reject(error)
+  }
 }
