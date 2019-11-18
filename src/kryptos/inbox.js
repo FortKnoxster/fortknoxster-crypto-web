6403d452794ed5bb1fb69cb4ee36e89ffbc6fe25
mickey <mj@fortknoxster.com>
2019-11-18 17:36:53 +0100

    Copyright header added

--- src/kryptos/inbox.js
+++ src/kryptos/inbox.js
@@ -1,3 +1,29 @@
+/**
+ * Copyright 2019 FortKnoxster Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @name Kryptos
+ * @file inbox.js
+ * @copyright Copyright © FortKnoxster Ltd. 2019.
+ * @license Apache License, Version 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ * @author Mickey Johnnysson <mj@fortknoxster.com>
+ * @author Christian Zwergius <cz@fortknoxster.com>
+ * @version 2.0
+ * @description Kryptos is a cryptographic library wrapping and implementing the
+ * Web Cryptography API. Kryptos supports symmetric keys and asymmetric key pair
+ * generation, key derivation, key wrap/unwrap, encryption, decryption, signing and verification.
+ */
 import { getPrivateKey, getPublicKey } from './serviceKeyStore'
 import { verifyDecrypt } from './decrypter'
 import { unwrapKey } from './keys'
